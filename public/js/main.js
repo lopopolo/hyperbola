a36dbecec2d8687b3f0298af06819b87154cabe4
lopopolo <rjl@hyperbo.la>
2010-08-07 23:13:22 +0000

    removed debugging. cleaned up this file.

--- public/js/main.js
+++ public/js/main.js
@@ -3,30 +3,16 @@ $(document).ready(function() {
 	$('#content_pane').scale9Grid({top:40,bottom:40,left:40,right:40});
 	
 //	// listener for url change
-//	window.addEventListener("hash_change", change_content, false); //false to get it in bubble
-//	fireGlobalEvent("hash_change");
 	$('#header a').click(function(event) {
 		event.preventDefault();
 		$.history.load($(this).html())
 	});
-	
-	$.history.init(change_content)
+	$.history.init(change_content);
 	
 });
 
-function fireGlobalEvent(name) {
-	//Ready: create a generic event
-	var evt = document.createEvent("Events")
-	//Aim: initialize it to be the event we want
-	evt.initEvent(name, true, true); //true for can bubble, true for cancelable
-	//FIRE!
-	document.dispatchEvent(evt);
-}
-
 function change_content(hash) {
-	alert(hash);
 	$('#content').fadeOut('slow', function() {
-//		var hash = location.hash;
 		hash = hash.replace('#', '');
 		switch (hash) {
 		case 'contact':
