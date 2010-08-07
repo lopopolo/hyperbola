d0aa41c82293e12a85fb7e75edf1b5af32917db9
lopopolo <rjl@hyperbo.la>
2010-08-07 22:39:05 +0000

    tweaked 9scale border

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	// 9grid the background bezel
-	$('#content_pane').scale9Grid({top:30,bottom:30,left:30,right:30});
+	$('#content_pane').scale9Grid({top:40,bottom:40,left:40,right:40});
 	
 	// listener for url change
 	window.addEventListener("hash_change", change_content, false); //false to get it in bubble
