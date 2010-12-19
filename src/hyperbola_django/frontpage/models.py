e47ae59ca1446d1189b31e9c7f6f81d0d4285736
lopopolo <rjl@hyperbo.la>
2010-12-19 02:38:41 +0000

    added better url for lifestream news items

--- src/hyperbola_django/frontpage/models.py
+++ src/hyperbola_django/frontpage/models.py
@@ -46,7 +46,7 @@ def obj_saved(**kwargs):
     if "Contact" in str(kwargs['sender']) or "Resume" in str(kwargs['sender']):
         link = "/contact"
     elif "LifeStream" in str(kwargs['sender']):
-        link = "/lifestream"
+        link = "/lifestream/" + kwargs['instance'].pk + "/"
     
     update = SiteNewsItem(blurb=blurb, link=link)
     update.save()
