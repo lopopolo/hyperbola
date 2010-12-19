9d2bea3ac63fe3f0c5e5f9e912b3a9e08fde3dad
lopopolo <rjl@hyperbo.la>
2010-12-19 05:13:49 +0000

    added rss and atom syndication to lifestream

--- src/hyperbola_django/lifestream/urls.py
+++ src/hyperbola_django/lifestream/urls.py
@@ -4,10 +4,16 @@ Created on Dec 18, 2010
 @author: Ryan Lopopolo
 '''
 from django.conf.urls.defaults import patterns
+from hyperbola_django.lifestream.syndication import *
 
 urlpatterns = patterns('lifestream.views',
     (r'^$', 'page', {"page_num" : 1}),
     (r'^page/(?P<page_num>\d+)/$', 'page'),
     (r'^archive/(?P<year>\d+)/(?P<month>\d+)/$', 'archive'),
     (r'^(?P<id>\d+)/$', 'permalink'),
+)
+
+urlpatterns += patterns('',
+    (r'^rss/$', LatestEntriesFeed()),
+    (r'^atom/$', AtomLatestEntriesFeed()),
 )
