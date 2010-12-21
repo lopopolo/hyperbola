47c82da492b82dad43078d442c047c37a6c6694c
lopopolo <rjl@hyperbo.la>
2010-12-21 07:23:45 +0000

    stupid unnecessary conditional escape

--- src/hyperbola_django/lifestream/templatetags/hashtagize.py
+++ src/hyperbola_django/lifestream/templatetags/hashtagize.py
@@ -5,7 +5,6 @@ Created on Dec 20, 2010
 '''
 import re
 from django.utils.safestring import mark_safe
-from django.utils.html import conditional_escape
 from django.template import Library
 
 register = Library()
@@ -13,20 +12,14 @@ register = Library()
 TAG_BASE = "/lifestream/hashtag/"
 
 @register.filter()
-def hashtagize(blurb, linktext=None, autoescape=None):
-    if autoescape:
-        esc = conditional_escape
-    else:
-        esc = lambda x: x
-    print blurb
+def hashtagize(blurb):
     
     blurb = re.sub('(?P<space>\s)#',
                    ' \g<space>#', 
-                   esc(blurb))
-    
+                   blurb)
     blurb = re.sub('(?P<space>\s)#(?P<tag>[^\s]+)(?P<end_space>\s)',
                    '\g<space><a href="' + TAG_BASE + '\g<tag>/">#\g<tag></a>\g<end_space>', 
-                   esc(blurb))
+                   blurb)
     blurb = re.sub('(?P<space>\s)#(?P<tag>[^\s]+)$',
                    '\g<space><a href="' + TAG_BASE + '\g<tag>/">#\g<tag></a>', 
                    blurb)
@@ -34,4 +27,3 @@ def hashtagize(blurb, linktext=None, autoescape=None):
                    '<a href="' + TAG_BASE + '\g<tag>/">#\g<tag></a>\g<end_space>', 
                    blurb)
     return mark_safe(blurb)
-hashtagize.needs_autoescape = True
