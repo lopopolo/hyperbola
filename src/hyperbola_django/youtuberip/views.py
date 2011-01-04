1fa8a2575506391913b1d740d2a02c7d38652e97
lopopolo <rjl@hyperbo.la>
2011-01-04 22:14:14 +0000

    split commands into lists

--- src/hyperbola_django/youtuberip/views.py
+++ src/hyperbola_django/youtuberip/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def video(request, id):
-    subprocess.call(DL_CMD + '"' + id + '" "' + YOUTUBE_URL + id + '"')
-    subprocess.call(FFMPEG_CMD.replace('FILENAME', id))
-    subprocess.call('mv "%s.mp3" %s' % (id, MP3_DIR))
+    subprocess.call(['./youtube-dl', '-o', '"%s"' % (id), '"%s%s"' % (YOUTUBE_URL, id)])
+    ffm = FFMPEG_CMD.replace('FILENAME', id)
+    ff = ffm.split()
+    subprocess.call(ff)
+    subprocess.call(['mv', '"%s.mp3" %s' % (id, MP3_DIR)])
     return HttpResponse('http://media.hyperbo.la/mp3s/%s.mp3' % (id))
