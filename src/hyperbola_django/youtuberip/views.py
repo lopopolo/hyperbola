0fe5320182f5eb120db30246359703c76dc55a57
lopopolo <rjl@hyperbo.la>
2011-01-04 22:07:35 +0000

    tweaks

--- src/hyperbola_django/youtuberip/views.py
+++ src/hyperbola_django/youtuberip/views.py
@@ -1,11 +1,11 @@
 # Create your views here.
 from settings import *
-from os import system
+import subprocess
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def video(request, id):
-    system(DL_CMD + id + ' ' + YOUTUBE_URL + id)
-    system(FFMPEG_CMD.replace('FILENAME', id))
-    system('mv %s.mp3 %s' % (id, MP3_DIR))
+    subprocess.call(DL_CMD + '"' + id + '" "' + YOUTUBE_URL + id + '"')
+    subprocess.call(FFMPEG_CMD.replace('FILENAME', id))
+    subprocess.call('mv "%s.mp3" %s' % (id, MP3_DIR))
     return HttpResponse('http://media.hyperbo.la/mp3s/%s.mp3' % (id))
