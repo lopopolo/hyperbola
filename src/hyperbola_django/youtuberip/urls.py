7f6e38eed92bca18cc78dc36be377e0835bdfb32
lopopolo <rjl@hyperbo.la>
2011-01-04 22:15:03 +0000

    forgot stupid quantifier

--- src/hyperbola_django/youtuberip/urls.py
+++ src/hyperbola_django/youtuberip/urls.py
@@ -6,6 +6,6 @@ Created on Jan 4, 2011
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('hyperbola_django.youtuberip.views',
-    (r'^video/(?P<id>[a-zA-Z_0-9])/$', 'video'),
+    (r'^video/(?P<id>[a-zA-Z_0-9]+)/$', 'video'),
     
 )
