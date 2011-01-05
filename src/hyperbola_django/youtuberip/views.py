d97bb6086529cea25082d491733678dc1db39392
lopopolo <rjl@hyperbo.la>
2011-01-05 00:20:12 +0000

    added youtube info

--- src/hyperbola_django/youtuberip/views.py
+++ src/hyperbola_django/youtuberip/views.py
@@ -3,11 +3,26 @@ from settings import *
 import subprocess
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-import os
+
+import gdata.youtube
+import gdata.youtube.service
+
+yt_service = gdata.youtube.service.YouTubeService()
+
+# The YouTube API does not currently support HTTPS/SSL access.
+yt_service.ssl = False
+
+
 def video(request, id):
     subprocess.call(['/home/lopopolo/hyperbola/hyperbola_django/youtuberip/youtube-dl', '-o', '/tmp/%s' % (id), '%s%s' % (YOUTUBE_URL, id)])
     ffm = FFMPEG_CMD.replace('FILENAME', '/tmp/'+id)
     ff = ffm.split()
     subprocess.call(ff)
     subprocess.call(['mv', '/tmp/%s.mp3' % (id), '%s/%s.mp3' % (MP3_DIR, id)])
-    return HttpResponse('http://media.hyperbo.la/mp3s/%s.mp3' % (id))
+    
+    entry = yt_service.GetYouTubeVideoEntry(video_id='the0KZLEacs')
+    
+    return render_to_response("json.html", {"link" : 'http://media.hyperbo.la/mp3s/%s.mp3' % (id),
+                                           "title" : entry.media.title.text,
+                                           "bpm" : 100,
+                                           "duration" : entry.media.duration.seconds})
