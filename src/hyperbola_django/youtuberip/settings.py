d8a0363857ce214c00024a50980eb042e258642a
lopopolo <rjl@hyperbo.la>
2011-01-05 00:08:54 +0000

    jesus that siucked

--- src/hyperbola_django/youtuberip/settings.py
+++ src/hyperbola_django/youtuberip/settings.py
@@ -8,6 +8,6 @@ MP3_DIR = '/var/www/hyperbola/media/mp3s'
 
 DL_CMD = './youtube-dl -o ' # video url
 
-FFMPEG_CMD = 'ffmpeg -i "FILENAME" -f mp3 -ab 160k -ar 44100 "FILENAME.mp3"'
+FFMPEG_CMD = 'ffmpeg -i FILENAME -f mp3 -ab 160k -ar 44100 FILENAME.mp3'
 
 YOUTUBE_URL = 'http://www.youtube.com/watch?v='
--- src/hyperbola_django/youtuberip/views.py
+++ src/hyperbola_django/youtuberip/views.py
@@ -3,11 +3,11 @@ from settings import *
 import subprocess
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-
+import os
 def video(request, id):
-    subprocess.call(['/home/lopopolo/hyperbola/hyperbola_django/youtuberip/youtube-dl', '-o', '%s' % (id), '%s%s' % (YOUTUBE_URL, id)])
-    ffm = FFMPEG_CMD.replace('FILENAME', id)
+    subprocess.call(['/home/lopopolo/hyperbola/hyperbola_django/youtuberip/youtube-dl', '-o', '/tmp/%s' % (id), '%s%s' % (YOUTUBE_URL, id)])
+    ffm = FFMPEG_CMD.replace('FILENAME', '/tmp/'+id)
     ff = ffm.split()
     subprocess.call(ff)
-    subprocess.call(['mv', '"%s.mp3" %s' % (id, MP3_DIR)])
+    subprocess.call(['mv', '/tmp/%s.mp3' % (id), '%s/%s.mp3' % (MP3_DIR, id)])
     return HttpResponse('http://media.hyperbo.la/mp3s/%s.mp3' % (id))
