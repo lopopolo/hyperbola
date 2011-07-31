510a824fd9e93f87c8eb685dec9e3fbb56dd7fff
Ryan Lopopolo <rjl@hyperbo.la>
2011-07-31 05:41:43 -0400

    Trashed Site News portion of front page

--- hyperbola/frontpage/models.py
+++ hyperbola/frontpage/models.py
@@ -1,84 +1,14 @@
 from django.db import models
-from django.db.models.signals import post_save, post_delete
-from hyperbola_django.contact.models import EmailContact, PhoneContact, WebContact, IMContact, Resume
-from hyperbola_django.lifestream.models import LifeStreamItem, LifeStreamPicture
 
 # Create your models here.
 class Blurb(models.Model):
-    title = models.CharField(max_length=200)
-    blurb = models.TextField()
-    display_order = models.IntegerField(unique=True)
+  title = models.CharField(max_length=200)
+  blurb = models.TextField()
+  display_order = models.IntegerField(unique=True)
+
+  class Meta:
+    ordering = ['display_order']
     
-    class Meta:
-        ordering = ['display_order']
-        
-    def __unicode__(self):
-        return "%s - %s" % (self.display_order, self.title)
-        
-class SiteNewsItem(models.Model):
-    pub_date = models.DateTimeField(auto_now=True)
-    link = models.CharField(max_length=200, blank=True)
-    blurb = models.CharField(max_length=200)
-    
-    class Meta:
-        ordering = ["-pub_date"]
-        
-    def __unicode__(self):
-        return self.blurb
-        
+  def __unicode__(self):
+    return "%s - %s" % (self.display_order, self.title)
 
-what = {EmailContact : "contact email",
-            PhoneContact : "phone number",
-            WebContact : "web presence",
-            IMContact : "IM contact",
-            Resume : "resume",
-            Blurb : "site blurb",
-            LifeStreamItem : "lifestream entry",
-            LifeStreamPicture : "lifestream picture"}
-
-def obj_saved(**kwargs):
-    blurb = ""
-    link = ""
-    if kwargs['created']:
-        blurb += "New %s added." % (what[kwargs['sender']])
-    else:
-        blurb += "%s updated." % (what[kwargs['sender']])
-    if "Contact" in str(kwargs['sender']) or "Resume" in str(kwargs['sender']):
-        link = "/contact/"
-    elif "LifeStream" in str(kwargs['sender']):
-        link = "/lifestream/" + str(kwargs['instance'].pk) + "/"
-    
-    update = SiteNewsItem(blurb=blurb, link=link)
-    update.save()
-
-def obj_deleted(**kwargs):
-    blurb = ""
-    link = ""
-    blurb += "%s deleted." % (what[kwargs['sender']])
-    if "Contact" in str(kwargs['sender']) or "Resume" in str(kwargs['sender']):
-        link = "/contact"
-    elif "LifeStream" in str(kwargs['sender']):
-        link = "/lifestream"
-    
-    update = SiteNewsItem(blurb=blurb, link=link)
-    update.save()
-
-post_save.connect(obj_saved, sender=EmailContact)
-post_save.connect(obj_saved, sender=PhoneContact)
-post_save.connect(obj_saved, sender=WebContact)
-post_save.connect(obj_saved, sender=IMContact)
-post_save.connect(obj_saved, sender=Blurb)
-post_save.connect(obj_saved, sender=LifeStreamItem)
-post_save.connect(obj_saved, sender=LifeStreamPicture)
-
-post_delete.connect(obj_deleted, sender=EmailContact)
-post_delete.connect(obj_deleted, sender=PhoneContact)
-post_delete.connect(obj_deleted, sender=WebContact)
-post_delete.connect(obj_deleted, sender=IMContact)
-post_delete.connect(obj_deleted, sender=Blurb)
-post_delete.connect(obj_deleted, sender=LifeStreamItem)
-post_delete.connect(obj_deleted, sender=LifeStreamPicture)
-
-
-    
-    
--- hyperbola/frontpage/views.py
+++ hyperbola/frontpage/views.py
@@ -3,15 +3,7 @@ from django.shortcuts import render_to_response
 
 # Create your views here.
 def index(request):
-    all_blurbs = Blurb.objects.all()
-    blurbs = []
-    for blurb in all_blurbs:
-        blurbs.append((blurb.title, blurb.blurb))
-    
-    most_recent_news = SiteNewsItem.objects.all()[:5]
-    news = []
-    for recent_news in most_recent_news:
-        news.append((recent_news.blurb, recent_news.link, recent_news.pub_date))
-        
-    return render_to_response("frontpage.html", {'blurbs' : blurbs,
-                                          'news' : news})
+  all_blurbs = Blurb.objects.all()
+  blurbs = [(b.title, b.blurb) for b in all_blurbs]
+
+  return render_to_response("frontpage.html", { "blurbs" : blurbs })
