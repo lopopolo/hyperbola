f1dc0572415b1f019a969985ec23d001d589d594
Ryan Lopopolo <rjl@hyperbo.la>
2011-07-31 04:59:00 -0400

    Rework settings

--- hyperbola/settings.py
+++ hyperbola/settings.py
@@ -1,23 +1,31 @@
-# Django settings for hyperbola_django project.
+# Django settings for hyperbola
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+import os
+import socket
+
+# Set DEBUG = True if on the production server
+if socket.gethostname() == 'hyperbox':
+  DEBUG = False
+else:
+  DEBUG = True
+
+# This dynamically discovers the path to the project
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+REPO_PATH = os.path.join(PROJECT_PATH, '..')
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+  ('Ryan Lopopolo', 'rjl@hyperbo.la'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\webdev\\hyperbola_django\\src\\sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'OPTIONS' : {
+      'read_default_file': os.path.join(PROJECT_PATH, 'db.cnf'),
+    },
+  }
 }
 
 # Local time zone for this installation. Choices can be found here:
@@ -45,62 +53,59 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = 'c:/webdev/hyperbola_django/src/hyperbola_django/media'
+MEDIA_ROOT = os.path.join(REPO_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://media.hyperbo.la/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+ADMIN_MEDIA_PREFIX = 'http://assets.hyperbo.la/django-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'y$6axl8$tcm)&h!s2i0y3dvy8fdyz(mty&_!w3pr@5)if-rj@2'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+  'django.template.loaders.filesystem.Loader',
+  'django.template.loaders.app_directories.Loader',
+  # 'django.template.loaders.eggs.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-        'django.core.context_processors.debug',
+  'django.core.context_processors.debug',
+  'django.contrib.auth.context_processors.auth',
 )
-
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'hyperbola_django.urls'
+ROOT_URLCONF = 'hyperbola.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "c:/webdev/hyperbola_django/src/hyperbola_django/templates",
+  # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+  # Always use forward slashes, even on Windows.
+  # Don't forget to use absolute paths, not relative paths.
+  os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-    'contact',
-    'frontpage',
-    'lifestream',
-    'appstore',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.sites',
+  'django.contrib.messages',
+  'django.contrib.admin',
+  # 'django.contrib.admindocs',
+  'hyperbola.contact',
+  'hyperbola.frontpage',
+  'hyperbola.lifestream',
 )
