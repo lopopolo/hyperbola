47acea7a202827d513717e27ec8a6af6b4d3ef4e
Ryan Lopopolo <rjl@hyperbo.la>
2011-07-31 21:12:07 -0700

    Do things the django way: all processing is in view. Only export lists and tuples to the template

--- hyperbola/contact/views.py
+++ hyperbola/contact/views.py
@@ -10,10 +10,10 @@ def index(request):
   all_phone = PhoneContact.objects.all()
   all_web = WebContact.objects.all()
   all_im = IMContact.objects.all()
-  contact_types = ContactType.objects.all()
-  all_contacts = dict([(type, []) for type in contact_types])
-  contact_order = dict([(type.display_order, type) for type in 
-    contact_types])
+  contact_categories = ContactType.objects.all()
+  all_contacts = dict([(category, []) for category in contact_categories])
+  contact_order = dict([(category.display_order, category) for category in 
+    contact_categories])
   for email in all_email:
     all_contacts[email.type].append((email.name, email.value))
   for phone in all_phone:
@@ -22,6 +22,12 @@ def index(request):
     all_contacts[web.type].append((web.name, '<a href="'+web.value+'">'+web.value+'</a>'))
   for im in all_im:
     all_contacts[im.type].append((im.name, im.value))
+  # prepare to ship to view
+  grouped_and_ordered_contacts = []
+  for order in sorted(contact_order.keys()):
+    category = contact_order[order]
+    grouped_and_ordered_contacts.append((category.type, all_contacts[category]))
+  # Resume is always last
   if Resume.objects.count() > 0:
     resume_as_of = Resume.objects.all()[0].date.strftime("%b %d %Y")
     all_contacts[u"Résumé"] = [(u"As of " + resume_as_of,
@@ -29,7 +35,7 @@ def index(request):
 
   return render_to_response("contact_base.html",
       { "name" : "Ryan Lopopolo", "contacts" : all_contacts,
-        "display_order" : contact_order, "about" : about() })
+        "about" : about() })
  
 def about():
   about_me = None
