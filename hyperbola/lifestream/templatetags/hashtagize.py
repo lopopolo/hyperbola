d7dbd5dbda9ad58a975b58c3307c90773d0e8a55
Ryan Lopopolo <rjl@hyperbo.la>
2011-08-01 00:43:55 -0400

    Fixed bug where hashtags that started a blurb weren't being converted

--- hyperbola/lifestream/templatetags/hashtagize.py
+++ hyperbola/lifestream/templatetags/hashtagize.py
@@ -11,6 +11,9 @@ def hashtagize(blurb):
   blurb = re.sub('(?P<leader>[^&])#(?P<tag>[A-Za-z0-9]+)',
       '<a href="' + TAG_BASE + '\g<tag>/">\g<leader>#\g<tag></a>',
       blurb)
+  blurb = re.sub('^#(?P<tag>[A-Za-z0-9]+)',
+      '<a href="' + TAG_BASE + '\g<tag>/">#\g<tag></a>',
+      blurb)
 
   return mark_safe(blurb)
 
