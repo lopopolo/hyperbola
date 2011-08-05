434eaa34aec896afc0c1f9b4841a96b5dfafb301
Ryan Lopopolo <rjl@hyperbo.la>
2011-08-05 02:32:44 -0400

    Added markup and admin docs to installed apps

--- hyperbola/urls.py
+++ hyperbola/urls.py
@@ -6,10 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-  # Uncomment the admin/doc line below to enable admin documentation:
-  # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-  # Uncomment the next line to enable the admin:
+  (r'^ssb/doc/', include('django.contrib.admindocs.urls')),
   (r'^ssb/', include(admin.site.urls)),
   (r'^$', include('hyperbola.frontpage.urls')),
   (r'^contact/', include('hyperbola.contact.urls')),
