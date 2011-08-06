bc1a9826e5cbe9950ee4007762ebd70b424be113
Ryan Lopopolo <rjl@hyperbo.la>
2011-08-06 18:48:24 -0400

    No more hard-coded urls!

--- hyperbola/lifestream/templatetags/hashtagize.py
+++ hyperbola/lifestream/templatetags/hashtagize.py
@@ -1,19 +1,26 @@
 import re
 from django.utils.safestring import mark_safe
 from django.template import Library
+from django.core.urlresolvers import reverse
+from hyperbola.lifestream import views
 
 register = Library()
 
-TAG_BASE = "/lifestream/hashtag/"
+def linkify(matchobj):
+  url = reverse(views.tag_page, args=[matchobj.group("tag")])
+  link_tag = ""
+  if "leader" in matchobj.groupdict():
+    link_tag += matchobj.group("leader")
+  link_tag += '<a href="%s">#%s</a>' % (url, matchobj.group("tag"))
+  return link_tag
 
 @register.filter()
 def hashtagize(blurb): 
-  blurb = re.sub('(?P<leader>[^&])#(?P<tag>[A-Za-z0-9]+)',
-      '\g<leader><a href="' + TAG_BASE + '\g<tag>/">#\g<tag></a>',
-      blurb)
-  blurb = re.sub('^#(?P<tag>[A-Za-z0-9]+)',
-      '<a href="' + TAG_BASE + '\g<tag>/">#\g<tag></a>',
-      blurb)
-
+  """
+  Use this filter to turn #hashtags into links
+  """
+  blurb = re.sub('(?P<leader>[^&])#(?P<tag>[A-Za-z0-9]+)', linkify, blurb)
+  # get hashtags that start a blurb 
+  blurb = re.sub('^#(?P<tag>[A-Za-z0-9]+)', linkify, blurb)
   return mark_safe(blurb)
 
