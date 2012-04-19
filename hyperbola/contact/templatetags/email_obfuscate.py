74362ece6cd71c6ab2d4b6b07757df7462e87b56
Ryan Lopopolo <rjl@hyperbo.la>
2012-04-19 02:24:02 -0400

    Fix html output to conform to a code linter
    http://lint.brihten.com/html/

--- hyperbola/contact/templatetags/email_obfuscate.py
+++ hyperbola/contact/templatetags/email_obfuscate.py
@@ -10,7 +10,7 @@ def obfuscate(email, linktext=None, autoescape=None):
   """
   Given a string representing an email address,
   returns a mailto link with rot13 JavaScript obfuscation.
-  
+
   Accepts an optional argument to use as the link text;
   otherwise uses the email address itself.
   """
@@ -28,10 +28,13 @@ def obfuscate(email, linktext=None, autoescape=None):
     else:
       linktext = email
 
-    rotten_link = """<script type="text/javascript">document.write \
-      ("<n uers=\\\"znvygb:%s\\\">%s<\\057n>".replace(/[a-zA-Z]/g, \
-      function(c){return String.fromCharCode((c<="Z"?90:122)>=\
-      (c=c.charCodeAt(0)+13)?c:c-26);}));</script>""" % (email, linktext)
+    rotten_link = """<script type="text/javascript">
+    //<![CDATA[
+    document.write('<n uers="znvygb:%s">%s</n>'.replace(/[a-zA-Z]/g,
+      function(c) {
+        return String.fromCharCode((c<="Z"?90:122) >= (c=c.charCodeAt(0)+13)?c:c-26);}));
+    //]]>
+    </script>""" % (email, linktext)
     return mark_safe(rotten_link)
   else:
     return mark_safe(email)
