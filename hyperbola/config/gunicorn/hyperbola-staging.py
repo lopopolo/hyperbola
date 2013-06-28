b40376b5bd05abbc9d9ca81e77d762d0117c1ee4
Ryan Lopopolo <rjl@hyperbo.la>
2013-06-28 09:22:51 -0400

    DEBUG loglevel on staging

--- hyperbola/config/gunicorn/hyperbola-staging.py
+++ hyperbola/config/gunicorn/hyperbola-staging.py
@@ -8,3 +8,4 @@ proc_name = 'staging.hyperbo.la'
 
 accesslog = '/hyperbola/var/log/gunicorn/hyperbola-staging.access.log'
 errorlog  = '/hyperbola/var/log/gunicorn/hyperbola-staging.error.log'
+loglevel  = 'debug'
