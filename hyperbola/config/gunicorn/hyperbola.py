27ff9205b9320642e4fdd86b2302945b2123dcb7
Ryan Lopopolo <rjl@hyperbo.la>
2013-06-28 08:28:09 -0400

    Hyperbola live gunicorn config

---
+++ hyperbola/config/gunicorn/hyperbola.py
@@ -0,0 +1,10 @@
+import multiprocessing
+
+bind = '127.0.0.1:7777'
+workers = multiprocessing.cpu_count() * 2 + 1
+
+pidfile = '/var/run/gunicorn_hyperbola.rjl.pid'
+proc_name = 'live.hyperbo.la'
+
+accesslog = '/hyperbola/var/log/gunicorn/hyperbola-live.access.log'
+errorlog  = '/hyperbola/var/log/gunicorn/hyperbola-live.error.log'
