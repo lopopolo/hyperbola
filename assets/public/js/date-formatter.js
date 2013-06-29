bc46d0076fc8f98fc8f37cb00bf56ac17704cb0d
Ryan Lopopolo <rjl@hyperbo.la>
2013-06-29 04:55:36 -0400

    Synchronous date localization
    Waiting for the onload to fire to trigger dates is slow when loading images

--- assets/public/js/date-formatter.js
+++ assets/public/js/date-formatter.js
@@ -1,50 +1,36 @@
-function convertDates() {
-  var spans = document.getElementsByTagName("span");
-  for (var i=0; i < spans.length; i++) {
-    var span = spans[i];
-    if (span.className == "date") {
-      var d = new Date(1000*parseInt(span.getAttribute("utc-seconds")));
-      span.innerHTML = printDate(d);
+var HyperbolaDateFormatter = {
+  printDate : function(date) {
+    var ret = "";
+    ret += this.padTo2(date.getHours());
+    ret += ":";
+    ret += this.padTo2(date.getMinutes());
+    ret += " ";
+    ret += this.monthToNameMap[date.getMonth()];
+    ret += " ";
+    ret += this.padTo2(date.getDate());
+    ret += " ";
+    ret += date.getFullYear();
+    return ret;
+  },
+  padTo2 : function(num) {
+    if (num < 10 && num >= 0) {
+      return "0" + num;
     }
-  }
+    return "" + num;
+  },
+  monthToNameMap : {
+    0  : "jan",
+    1  : "feb",
+    2  : "mar",
+    3  : "apr",
+    4  : "may",
+    5  : "jun",
+    6  : "jul",
+    7  : "aug",
+    8  : "sep",
+    9  : "oct",
+    10 : "nov",
+    11 : "dec"
+  },
 }
 
-function printDate(date) {
-  var ret = "";
-  ret += padTo2(date.getHours());
-  ret += ":";
-  ret += padTo2(date.getMinutes());
-  ret += " ";
-  //ret += "tzname";
-  //ret += " ";
-  ret += monthToNameMap[date.getMonth()];
-  ret += " ";
-  ret += padTo2(date.getDate());
-  ret += " ";
-  ret += date.getFullYear();
-  return ret;
-}
-
-function padTo2(num) {
-  if (num < 10 && num >= 0) {
-    return "0" + num;
-  }
-  return "" + num;
-}
-
-var monthToNameMap = {
-  0  : "jan",
-  1  : "feb",
-  2  : "mar",
-  3  : "apr",
-  4  : "may",
-  5  : "jun",
-  6  : "jul",
-  7  : "aug",
-  8  : "sep",
-  9  : "oct",
-  10 : "nov",
-  11 : "dec"
-}
-
-window.onload = convertDates;
