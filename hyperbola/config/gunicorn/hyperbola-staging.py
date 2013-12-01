217d5412ab6c8bbe852bb4b8faceb85ef60b3f38
Ryan Lopopolo <rjl@hyperbo.la>
2013-12-01 06:12:52 -0500

    gunicorn config is flake8 compliant

--- hyperbola/config/gunicorn/hyperbola-staging.py
+++ hyperbola/config/gunicorn/hyperbola-staging.py
@@ -1,5 +1,3 @@
-import multiprocessing
-
 bind = '127.0.0.1:8080'
 workers = 1
 
@@ -7,5 +5,5 @@ pidfile = '/var/run/gunicorn_hyperbola-staging.rjl.pid'
 proc_name = 'staging.hyperbo.la'
 
 accesslog = '/hyperbola/var/log/gunicorn/hyperbola-staging.access.log'
-errorlog  = '/hyperbola/var/log/gunicorn/hyperbola-staging.error.log'
-loglevel  = 'debug'
+errorlog = '/hyperbola/var/log/gunicorn/hyperbola-staging.error.log'
+loglevel = 'debug'
--- hyperbola/config/gunicorn/hyperbola.py
+++ hyperbola/config/gunicorn/hyperbola.py
@@ -7,4 +7,4 @@ pidfile = '/var/run/gunicorn_hyperbola.rjl.pid'
 proc_name = 'live.hyperbo.la'
 
 accesslog = '/hyperbola/var/log/gunicorn/hyperbola-live.access.log'
-errorlog  = '/hyperbola/var/log/gunicorn/hyperbola-live.error.log'
+errorlog = '/hyperbola/var/log/gunicorn/hyperbola-live.error.log'
