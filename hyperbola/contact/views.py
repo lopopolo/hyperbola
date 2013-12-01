eed1a1050b05483014a8b780379b75e81d2c3d14
Ryan Lopopolo <rjl@hyperbo.la>
2013-11-30 20:03:13 -0500

    Refactor contacts view and template
    * use proper objects instead of tuples
    * properly escape data in template

--- hyperbola/contact/views.py
+++ hyperbola/contact/views.py
@@ -1,10 +1,88 @@
 # Create your views here.
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404
 from django.core.urlresolvers import reverse
 from models import *
+
+class ContactGroup(object):
+  def __init__(self, title, display_order=None):
+    self.title = title
+    self.display_order = display_order
+    self.contacts = []
+
+  def add_contact(self, contact):
+    self.contacts.append(contact)
+
+class ContactDTO(object):
+  def name(self):
+    pass
+  def value(self):
+    pass
+  def is_link(self):
+    return False
+  def link_text(self):
+    return None
+  def is_email(self):
+    return False
+
+class EmailDTO(ContactDTO):
+  def __init__(self, title, email):
+    self.title = title
+    self.email = email
+  def name(self):
+    return self.title
+  def value(self):
+    return self.email
+  def is_email(self):
+    return True
+
+class PhoneDTO(ContactDTO):
+  def __init__(self, title, phone):
+    self.title = title
+    self.phone = phone
+  def name(self):
+    return self.title
+  def value(self):
+    return self.phone
+
+class InstantMessageDTO(ContactDTO):
+  def __init__(self, title, nick):
+    self.title = title
+    self.nick = nick
+  def name(self):
+    return self.title
+  def value(self):
+    return self.nick
+
+class WebDTO(ContactDTO):
+  def __init__(self, title, url):
+    self.title = title
+    self.url = url
+  def name(self):
+    return self.title
+  def value(self):
+    return self.url
+  def is_url(self):
+    return True
+  def link_text(self):
+    return self.url
+
+class ResumeDTO(ContactDTO):
+  def __init__(self, date, url, request):
+    self.date = date
+    self.url = url
+    self.request = request
+  def name(self):
+    return "As of {0}".format(self.date.strftime("%b %d %Y"))
+  def value(self):
+    return self.url
+  def is_url(self):
+    return True
+  def link_text(self):
+    return self.request.build_absolute_uri(self.url)
+
 
 def index(request):
   all_email = EmailContact.objects.all()
@@ -12,44 +90,35 @@ def index(request):
   all_web = WebContact.objects.all()
   all_im = IMContact.objects.all()
   contact_categories = ContactType.objects.all()
-  all_contacts = dict([(category, []) for category in contact_categories])
-  contact_order = dict([(category.display_order, category) for category in
-    contact_categories])
+  all_contacts = { category : ContactGroup(category.type, category.display_order) for category in contact_categories }
   for email in all_email:
-    all_contacts[email.type].append((email.name, email.value))
+    all_contacts[email.type].add_contact(EmailDTO(email.name, email.value))
   for phone in all_phone:
-    all_contacts.append[phone.type]((phone.name, phone.value))
+    all_contacts[phone.type].add_contact(PhoneDTO(phone.name, phone.value))
   for web in all_web:
-    all_contacts[web.type].append((web.name, '<a href="{0}">{1}</a>'.format(web.value, web.value)))
+    all_contacts[web.type].add_contact(WebDTO(web.name, web.value))
   for im in all_im:
-    all_contacts[im.type].append((im.name, im.value))
+    all_contacts[im.type].add_contact(InstantMessageDTO(im.name, im.value))
   # prepare to ship to view
-  grouped_and_ordered_contacts = []
-  for order in sorted(contact_order.keys()):
-    category = contact_order[order]
-    if len(all_contacts[category]) > 0:
-      grouped_and_ordered_contacts.append((category.type, all_contacts[category]))
+  sorted_contact_groups = sorted(all_contacts.values(), key=lambda k: k.display_order)
+  filtered_contact_groups = [cg for cg in sorted_contact_groups if len(cg.contacts) > 0]
   # Resume is always last
   if Resume.objects.count() > 0:
+    resume_contact_group = ContactGroup(u"R\u00E9sum\u00E9")
+    latest_resume = Resume.objects.latest("date")
     resume_link = reverse(resume, args=[])
-    resume_as_of = Resume.objects.latest("date").date.strftime("%b %d %Y")
-    grouped_and_ordered_contacts.append((
-      u"Résumé", [
-        (u"As of " + resume_as_of,
-          '<a href="{0}">{1}</a>'.format(resume_link, request.build_absolute_uri(resume_link))
-        )
-      ]
-    ))
+    resume_contact_group.add_contact(ResumeDTO(latest_resume.date, resume_link, request=request))
+    filtered_contact_groups.append(resume_contact_group)
 
   return render_to_response("contact_base.html",
-      { "name" : "Ryan Lopopolo", "contacts" : grouped_and_ordered_contacts,
+      { "name" : "Ryan Lopopolo", "contacts" : filtered_contact_groups,
         "about" : about() })
 
 def about():
   about_me = None
   if AboutMe.objects.count() > 0:
     newest = AboutMe.objects.latest("id")
-    about_me = (newest.photo, newest.blurb)
+    about_me = newest
   return about_me
 
 def resume(request):
