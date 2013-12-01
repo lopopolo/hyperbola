6f8e0272518de2b0469b898a9fe013b8c2d8b553
Ryan Lopopolo <rjl@hyperbo.la>
2013-12-01 04:42:13 -0500

    root urls.py flake8 compliant

--- hyperbola/urls.py
+++ hyperbola/urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls import *
-from django.conf import settings
+from django.conf.urls import patterns, include
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-  (r'^ssb/doc/', include('django.contrib.admindocs.urls')),
-  (r'^ssb/', include(admin.site.urls)),
-  (r'^$', include('hyperbola.frontpage.urls')),
-  (r'^contact/', include('hyperbola.contact.urls')),
-  (r'^lifestream/', include('hyperbola.lifestream.urls')),
+urlpatterns = patterns(
+    '',
+    (r'^ssb/doc/', include('django.contrib.admindocs.urls')),
+    (r'^ssb/', include(admin.site.urls)),
+    (r'^$', include('hyperbola.frontpage.urls')),
+    (r'^contact/', include('hyperbola.contact.urls')),
+    (r'^lifestream/', include('hyperbola.lifestream.urls')),
 )
-
