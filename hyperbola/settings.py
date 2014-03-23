1effcca275042c01b9547740f590b61dc86dd1a0
Ryan Lopopolo <rjl@hyperbo.la>
2014-03-23 05:19:21 -0400

    Add django-email-obfuscator instead of rolling our own
    Encodes using ASCII character entities. Requires no JS.
    
    Previously, we were rot13'ing on the server and rot13'ing again
    in JS on the client.

--- hyperbola/settings.py
+++ hyperbola/settings.py
@@ -84,6 +84,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'email_obfuscator',
     'localflavor',
     'pipeline',
     'sorl.thumbnail',
