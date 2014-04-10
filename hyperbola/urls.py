3fb33e0241072cc297be96fc660fdb776c571a96
Ryan Lopopolo <rjl@hyperbo.la>
2014-04-10 05:47:08 -0400

    Add 404 route in django
    Add a route in django to display a 404 page. Set the
    404 error_page in nginx to point to this route.
    
    Fixes an issue where the stock nginx 404 page would
    display when attempting to access an internal location.

--- hyperbola/urls.py
+++ hyperbola/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,4 +12,5 @@ urlpatterns = patterns(
     (r'^$', include('hyperbola.frontpage.urls')),
     (r'^contact/', include('hyperbola.contact.urls')),
     (r'^lifestream/', include('hyperbola.lifestream.urls')),
+    (r'^404.html$', TemplateView.as_view(template_name='404.html')),
 )
