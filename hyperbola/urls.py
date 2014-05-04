8d0d45b1bb0cc71ed9461f73e8af3b0a40aeaf4a
Ryan Lopopolo <rjl@hyperbo.la>
2014-05-04 12:36:41 -0700

    Standardize django url style in root url conf

--- hyperbola/urls.py
+++ hyperbola/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import patterns, include
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import django.contrib.admindocs.urls
 
+import hyperbola.contact.urls
+import hyperbola.frontpage.urls
+import hyperbola.lifestream.urls
 from hyperbola.helpers.views import NotFound404View
 
 
@@ -8,10 +12,10 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    (r'^ssb/doc/', include('django.contrib.admindocs.urls')),
-    (r'^ssb/', include(admin.site.urls)),
-    (r'^$', include('hyperbola.frontpage.urls')),
-    (r'^contact/', include('hyperbola.contact.urls')),
-    (r'^lifestream/', include('hyperbola.lifestream.urls')),
-    (r'^404.html$', NotFound404View.as_view()),
+    url(r'^ssb/doc/', include(django.contrib.admindocs.urls)),
+    url(r'^ssb/', include(admin.site.urls)),
+    url(r'^$', include(hyperbola.frontpage.urls)),
+    url(r'^contact/', include(hyperbola.contact.urls)),
+    url(r'^lifestream/', include(hyperbola.lifestream.urls)),
+    url(r'^404.html$', NotFound404View.as_view()),
 )
