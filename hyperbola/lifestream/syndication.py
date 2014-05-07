699a3d9d1199ca7e82934c01718c08311c1021c9
Ryan Lopopolo <rjl@hyperbo.la>
2014-05-06 20:32:35 -0700

    Simplify Lifestream RSS feed

--- hyperbola/lifestream/syndication.py
+++ hyperbola/lifestream/syndication.py
@@ -9,21 +9,25 @@ from hyperbola.lifestream.models import LifeStreamItem
 
 class LatestEntriesFeed(Feed):
     title = "hyperbo.la lifestream microblog"
-    link = "/lifestream/"
+
     description = (
         "Most recent posts made to Ryan Lopopolo's "
         "lifestreaming microblog."
     )
 
+    description_template = "blurb_feed.html"
+
+    def link(self):
+        return reverse(views.lifestream_index)
+
+    def feed_url(self):
+        return reverse("lifestream-rss")
+
     def items(self):
-        return LifeStreamItem.objects.all().select_related('lifestreampicture')
+        return LifeStreamItem.objects.all().select_related("lifestreampicture")
 
     def item_title(self, item):
         return "Post #{0}".format(item.pk)
-
-    def item_description(self, item):
-        post = render_to_response("blurb.html", {"post": item})
-        return post.content
 
     def item_link(self, item):
         return reverse(views.permalink, args=[item.pk])
@@ -31,4 +35,8 @@ class LatestEntriesFeed(Feed):
 
 class AtomLatestEntriesFeed(LatestEntriesFeed):
     feed_type = Atom1Feed
+
     subtitle = LatestEntriesFeed.description
+
+    def feed_url(self):
+        return reverse("lifestream-atom")
