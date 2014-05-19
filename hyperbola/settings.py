3841d1193dbc5ae3daf4adc3f36fe9b9c9b14d2f
Ryan Lopopolo <rjl@hyperbo.la>
2014-05-19 00:54:58 -0400

    Move to a third-party library for markdown rendering

--- hyperbola/settings.py
+++ hyperbola/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'email_obfuscator',
     'localflavor',
+    'markdown_deux',
     'pipeline',
     'sorl.thumbnail',
     'hyperbola.contact',
