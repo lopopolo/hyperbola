7687051378787787b0bc61f643ad26717e65d580
Ryan Lopopolo <rjl@hyperbo.la>
2014-06-05 02:03:04 -0700

    Simplify upstart + gunicorn config
    Upstart:
    * Move to native upstart config options instead of using gunicorn options
    * chdir to current artifact before script starts (avoids a race condition
      of a deployment happening mid-restart).
    * Depend on python search path being configured by deploy process
    
    Gunicorn:
    * Remove pidfile conf, since this is not needed by upstart
    * Remove proc_name conf, since the python package to enable it
      is not installed.
    * Rename log files.

--- conf/gunicorn/hyperbola-staging.py
+++ conf/gunicorn/hyperbola-staging.py
@@ -1,9 +1,6 @@
 bind = 'unix:/hyperbola/var/sock/hyperbola-staging.sock'
 workers = 1
 
-pidfile = '/var/run/gunicorn_hyperbola-staging.rjl.pid'
-proc_name = 'staging.hyperbo.la'
-
-accesslog = '/hyperbola/var/log/gunicorn/hyperbola-staging.access.log'
-errorlog = '/hyperbola/var/log/gunicorn/hyperbola-staging.error.log'
+accesslog = '/hyperbola/var/log/gunicorn/staging.access'
+errorlog = '/hyperbola/var/log/gunicorn/staging.error'
 loglevel = 'debug'
--- conf/gunicorn/hyperbola.py
+++ conf/gunicorn/hyperbola.py
@@ -3,8 +3,5 @@ import multiprocessing
 bind = 'unix:/hyperbola/var/sock/hyperbola-live.sock'
 workers = multiprocessing.cpu_count() + 1
 
-pidfile = '/var/run/gunicorn_hyperbola.rjl.pid'
-proc_name = 'live.hyperbo.la'
-
-accesslog = '/hyperbola/var/log/gunicorn/hyperbola-live.access.log'
-errorlog = '/hyperbola/var/log/gunicorn/hyperbola-live.error.log'
+accesslog = '/hyperbola/var/log/gunicorn/live.access'
+errorlog = '/hyperbola/var/log/gunicorn/live.error'
