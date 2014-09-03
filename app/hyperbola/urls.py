77f71c354b6db10971ae0fb65c2fcb56666c604c
Ryan Lopopolo <rjl@hyperbo.la>
2014-09-02 20:39:02 -0700

    Only enable admin site urls in production

--- app/hyperbola/urls.py
+++ app/hyperbola/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import django.contrib.admindocs.urls
 
+from hyperbola import settings
 import hyperbola.contact.urls
 import hyperbola.frontpage.urls
 import hyperbola.lifestream.urls
@@ -12,10 +13,15 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^ssb/doc/', include(django.contrib.admindocs.urls)),
-    url(r'^ssb/', include(admin.site.urls)),
     url(r'^$', include(hyperbola.frontpage.urls)),
     url(r'^contact/', include(hyperbola.contact.urls)),
     url(r'^lifestream/', include(hyperbola.lifestream.urls)),
     url(r'^404.html$', NotFound404View.as_view()),
 )
+
+if settings.ENVIRONMENT == 'production':
+    # only enable admin urls in production
+    urlpatterns += (
+        url(r'^ssb/', include(admin.site.urls)),
+        url(r'^ssb/doc/', include(django.contrib.admindocs.urls)),
+    )
