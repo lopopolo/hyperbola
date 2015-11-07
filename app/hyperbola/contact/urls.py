8eebc9c7b7ea6e61d403c27412ce9fae4029dc79
Ryan Lopopolo <rjl@hyperbo.la>
2015-11-07 02:41:10 -0800

    Convert /resume/? redirect from a lambda to a view

--- app/hyperbola/contact/urls.py
+++ app/hyperbola/contact/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls import url
-from django.core.urlresolvers import reverse_lazy
-from django.http import HttpResponsePermanentRedirect
+from django.views.generic import RedirectView
 
 from hyperbola.contact import views
 
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^resume/lopopolo.pdf$', views.resume, name="resume-pdf"),
-    url(
-        r'^resume/?$',
-        lambda req: HttpResponsePermanentRedirect(reverse_lazy(views.resume))
-    ),
+    url(r'^resume/?$', RedirectView.as_view(pattern_name="resume-pdf",
+                                            permanent=True)),
 ]
