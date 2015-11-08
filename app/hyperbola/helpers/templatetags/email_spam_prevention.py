0b1e0b6eaa550ad5a9e8cf70e908b276c37df253
Ryan Lopopolo <rjl@hyperbo.la>
2015-11-07 21:44:37 -0800

    Embed a reimplementation of django-email-obfuscator
    This lib is abandoned and broken. I have to pin an old version.

---
+++ app/hyperbola/helpers/templatetags/email_spam_prevention.py
@@ -0,0 +1,35 @@
+import re
+
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@register.filter(needs_autoescape=True)
+@stringfilter
+def anti_spamize(email, autoescape=True):
+    """The anti_spamize filter turns email addresses into HTML
+     entity encoded mailto links.
+
+    Args:
+        email: The email to encode. The encoded email will be the link's
+            href value and text.
+        autoescape: Whether or not this template tag should escape blurb
+            before inserting hashtag link markup.
+    """
+    if autoescape:
+        def esc(x): return conditional_escape(x)
+    else:
+        def esc(x): return x
+
+    def encode(value):
+        return "".join(["&#x{code:x};".format(code=ord(char)) for char in value])
+
+    return mark_safe('<a href="{link}">{text}</a>'.format(
+        link=encode("mailto:{}".format(esc(email))),
+        text=encode(esc(email))
+    ))
