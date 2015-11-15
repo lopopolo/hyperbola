821623dd8eceb44ba7ba0fb0cda5bef982d4ab08
Ryan Lopopolo <rjl@hyperbo.la>
2015-11-15 06:52:52 -0800

    Enable django-debug-toolbar in dev

--- app/hyperbola/settings.py
+++ app/hyperbola/settings.py
@@ -196,14 +196,33 @@ elif ENVIRONMENT == 'staging':
     warnings.simplefilter('error', DeprecationWarning)
 elif ENVIRONMENT == 'dev':
     DEBUG = True
-    MEDIA_ROOT = os.path.join(ROOT_PATH, 'dev-media')
+    MEDIA_ROOT = os.path.join(ROOT_PATH, 'prod-media')
     MEDIA_URL = '/media/'
     PIPELINE_ENABLED = False
     STATIC_URL = '/static/'
     INSTALLED_APPS += (
         'django.contrib.admin',
         'django.contrib.admindocs',
+        'debug_toolbar',
+        'template_timings_panel',
+        'template_profiler_panel',
     )
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+        'template_profiler_panel.panels.template.TemplateProfilerPanel',
+    ]
 
     warnings.simplefilter('error', DeprecationWarning)
 else:
