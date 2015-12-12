ef1f5336117c6b9886bee0e6f82f076cbfd5fe4d
Ryan Lopopolo <rjl@hyperbo.la>
2015-12-11 23:51:16 -0800

    Properly refer to the root url in urls.py
    Silences system check under python -Wall

--- app/hyperbola/urls.py
+++ app/hyperbola/urls.py
@@ -10,7 +10,7 @@ from hyperbola.helpers.views import NotFound404View
 
 
 urlpatterns = [
-    url(r'^$', include(hyperbola.frontpage.urls)),
+    url(r'', include(hyperbola.frontpage.urls)),
     url(r'^contact/', include(hyperbola.contact.urls)),
     url(r'^lifestream/', include(hyperbola.lifestream.urls)),
     url(r'^404.html$', NotFound404View.as_view()),
