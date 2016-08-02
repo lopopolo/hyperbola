e9f07eeb979a03d78ffc92c00c544220dec0683e
Ryan Lopopolo <rjl@hyperbo.la>
2016-08-01 21:35:14 -0500

    Upgrade to django 1.10

--- app/hyperbola/settings.py
+++ app/hyperbola/settings.py
@@ -95,13 +95,12 @@ INSTALLED_APPS = [
     'hyperbola.lifestream',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
