6970b82709ef94bb50d658980906e627f7dea6d6
Ryan Lopopolo <rjl@hyperbo.la>
2016-11-13 04:32:37 -0600

    GH-39: Add redis as a cache backend
    * enable cache backend for sessions

--- app/hyperbola/settings.py
+++ app/hyperbola/settings.py
@@ -45,6 +45,19 @@ DATABASES = {
     }
 }
 
+if ENVIRONMENT in ['production', 'staging']:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django_redis.cache.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379/1',
+            'OPTIONS': {
+                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            }
+        }
+    }
+    SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+    SESSION_CACHE_ALIAS = 'default'
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
