dadd3342611a4e8c5c66800d5d02c28cb4e5554b
Ryan Lopopolo <rjl@hyperbo.la>
2017-05-13 01:24:30 -0500

    Migrate terraform state to remote S3 backend with dynamo locking

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,16 @@ variable "name" {}
 
 variable "iam_admins" {}
 
+terraform {
+  backend "s3" {
+    bucket     = "hyperbola-terraform-state"
+    region     = "us-east-1"
+    key        = "terraform/hyperbola/terraform.tfstate"
+    encrypt    = true
+    lock_table = "terraform_statelock"
+  }
+}
+
 resource "aws_key_pair" "site_key" {
   key_name   = "hyperbola-cas"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCahzyhyrx5VIHEJzw8s2s3J+YUbx3Wh31Jb3BG+0TYc7Bi9EjLLOXa0ttzSVsYsKViIB6+IqwJnIGt9XoKxm1jpP4jNudMlmr9Uda8TFwVRvdAK2JSO5q/lyQ6PVJ2LK7MByGN4Tw7cuqmyzeRgIg2zU3aIwqJxQdrK5ORuOG0ChmlS4CA5jI4F4xdUCrP7wNJ3JYR7OlZ/8R1PHHtSyoub8n2dv4n+HuzG6kq9B6pJCwPXCaotifvwuJv2Ezs+Ui0aDoZjsW6RWpVCBX/hQMRIZjCa3UOUhTU3Qzi8KmuVkXG5jFwFY+RlK5Ldu9kPnxRTBv5FFW1VQVYJmoQp2k345J0pV43rUobDm9QnHCgbPe2GZa/Defje+ihsf8eSrZGnVw3SFVKoZdb18oHfuZDdQ8g9cXYcZ4oRV0yURRFLy45V8g9VTQPO3SPaDzg6FK+1e3YLFG6eWQ6ldwE9wD+CR9NIMEpuNvN26AGgPZ9xon3IO1KgNs+2vs/BazcNVfj8oK8JgCRDWG0K4+KvsUYHxT6xDw8hdj5QhhokOJjPBzCraaCdweM0cNUWumnJd4dR70dtLl4rgj/kYWd7GPvB8EGDitmAjaaYWlxeAEZ6CVCrfiqJs7CkD58AUnsZB7a3bZHAlQA46qOYLSm6h5Q4wXL9D62d1ufQPZkNzybiQ== hyperbola-cas"
---
+++ terraform/state/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "s3" {
+    bucket     = "hyperbola-terraform-state"
+    region     = "us-east-1"
+    key        = "terraform/state-infra/terraform.tfstate"
+    encrypt    = true
+    lock_table = "terraform_statelock"
+  }
+}
+
+resource "aws_s3_bucket" "hyperbola_terraform_state" {
+  bucket = "hyperbola-terraform-state"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_statelock" {
+  name           = "terraform_statelock"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
