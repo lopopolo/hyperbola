2f58cb0a6db3c9c7ae229f16f46881d5922e033d
Ryan Lopopolo <rjl@hyperbo.la>
2017-07-03 15:05:12 -0700

    Convert lifestream date localization to time-elements web component
    Remove janky HyperbolaDateFormatter. Fixes GH-57.

--- src/main.js
+++ src/main.js
@@ -2,24 +2,5 @@
 
 require("hyperbola.browser");
 
-var localizeDate = function() {}; // noop
-if (exports.Intl && typeof exports.Intl === "object") {
-    var formatter = new exports.Intl.DateTimeFormat(undefined, {
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-        hour: "numeric",
-        minute: "2-digit",
-        timeZoneName: "short"
-    });
-    localizeDate = function(timestamp, selector) {
-        var d = new Date();
-        d.setTime(timestamp * 1000);
-        document.getElementById(selector).innerText = formatter.format(d);
-    };
-}
-
-module.exports = {
-    HyperbolaDateFormatter: localizeDate,
-    retinajs: require("retinajs/dist/retina.js"),
-};
+require("retinajs/dist/retina.js"),
+require("time-elements");
