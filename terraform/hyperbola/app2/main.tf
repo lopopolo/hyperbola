7222674b6aab4c64ae621dceb644d5cd264b6606
Ryan Lopopolo <rjl@hyperbo.la>
2017-07-05 17:15:10 -0700

    Add AAAA record for local.hyperbolausercontent.net

--- terraform/hyperbola/app2/main.tf
+++ terraform/hyperbola/app2/main.tf
@@ -6,10 +6,22 @@ data "aws_route53_zone" "hyperbolausercontent" {
   private_zone = false
 }
 
-resource "aws_route53_record" "cdn" {
+resource "aws_route53_record" "cdn-A" {
   zone_id = "${data.aws_route53_zone.hyperbolausercontent.zone_id}"
   name    = "${var.bucket}"
   type    = "A"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.cdn.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cdn-AAAA" {
+  zone_id = "${data.aws_route53_zone.hyperbolausercontent.zone_id}"
+  name    = "${var.bucket}"
+  type    = "AAAA"
 
   alias {
     name                   = "${aws_cloudfront_distribution.cdn.domain_name}"
