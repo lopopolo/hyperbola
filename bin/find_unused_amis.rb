c8d5f91346e438b7b65864873a735b62d5b35b24
Ryan Lopopolo <rjl@hyperbo.la>
2017-07-17 00:46:07 -0700

    Enable AMI management scripts to work in us-east-1 and us-west-2

--- bin/find_unused_amis.rb
+++ bin/find_unused_amis.rb
@@ -3,24 +3,29 @@
 
 require 'aws-sdk-v1'
 
-client = AWS::EC2.new
-amis = client.instances.map do |instance|
-  begin
-    instance.image_id
-  rescue AWS::Core::Resource::NotFound
-    nil
+regions = ["us-east-1", "us-west-2"]
+
+regions.each do |region|
+  puts "# #{region}"
+  client = AWS::EC2.new(region: region)
+  amis = client.instances.map do |instance|
+    begin
+      instance.image_id
+    rescue AWS::Core::Resource::NotFound
+      nil
+    end
+  end.compact.uniq
+
+  puts '# Loading available AMIs...'
+  images = client.images.with_owner('self').sort_by(&:name)
+  images.each do |image|
+    next if amis.include?(image.id)
+    fields = [
+      image.id,
+      image.block_device_mappings.values.map(&:snapshot_id).compact.join(','),
+      image.name
+    ]
+
+    puts(fields.join("\t"))
   end
-end.compact.uniq
-
-puts '# Loading available AMIs...'
-images = client.images.with_owner('self').sort_by(&:name)
-images.each do |image|
-  next if amis.include?(image.id)
-  fields = [
-    image.id,
-    image.block_device_mappings.values.map(&:snapshot_id).compact.join(','),
-    image.name
-  ]
-
-  puts(fields.join("\t"))
 end
