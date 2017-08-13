27b017d22335a263a3691146efefceb9a4fdaf28
Ryan Lopopolo <rjl@hyperbo.la>
2017-08-12 20:05:57 -0700

    Make ansible_vault_password.sh easier to understand
    just a little code duplication

--- bin/ansible_vault_password.sh
+++ bin/ansible_vault_password.sh
@@ -2,7 +2,22 @@
 
 unset CDPATH
 
-BIN_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ARTIFACT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-# shellcheck disable=SC2016
-exec "$BIN_ROOT/artifact-exec" "bash" "-c" '[[ -n "$ANSIBLE_VAULT_PASSWORD" ]] && echo "$ANSIBLE_VAULT_PASSWORD"'
+if [[ ! -d "$ARTIFACT_ROOT/env" ]]; then
+	echo >&2 "No env directory to source"
+	echo >&2 "ARTIFACT_ROOT=$ARTIFACT_ROOT"
+	exit 1
+fi
+
+for env in "$ARTIFACT_ROOT/env/"*.env; do
+	# shellcheck source=/dev/null
+	. "$env"
+done
+
+if [[ -z "$ANSIBLE_VAULT_PASSWORD" ]]; then
+	echo &>2 "ANSIBLE_VAULT_PASSWORD not set"
+	exit 1
+fi
+
+echo "$ANSIBLE_VAULT_PASSWORD"
