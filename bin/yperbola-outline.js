ae6ca2f94ef2ab2db4c4b1e66923b65fcf74e06d
Ryan Lopopolo <rjl@hyperbo.la>
2017-11-01 20:59:50 -0700

    Make linter pass pre-commit run --all-files eslint

--- bin/yperbola-outline.js
+++ bin/yperbola-outline.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
 const { Console } = console;
+const TextToSVG = require('text-to-svg');
+
 const out = new Console(process.stdout, process.stderr);
-const TextToSVG = require("text-to-svg");
-const textToSVG = TextToSVG.loadSync("/Library/Fonts/Brush Script.ttf");
+const textToSVG = TextToSVG.loadSync('/Library/Fonts/Brush Script.ttf');
 
-const attributes = {fill: "#28dcdc", stroke: "#28dcdc"};
-const options = {fontSize: 40, anchor: "middle", attributes: attributes};
+const attributes = { fill: '#28dcdc', stroke: '#28dcdc' };
+const options = { fontSize: 40, anchor: 'middle', attributes };
 
-out.warn(textToSVG.getMetrics("yperbola", options));
-const svg = textToSVG.getPath("yperbola", options);
+out.warn(textToSVG.getMetrics('yperbola', options));
+const svg = textToSVG.getPath('yperbola', options);
 
 out.log(svg);
