0338925c5e0baf98b40120b34d62870ee687486e
Ryan Lopopolo <rjl@hyperbo.la>
2017-11-03 21:47:31 -0700

    Turn off aurora mysql dns

--- terraform/env/app-prod-northwest/dns.tf
+++ terraform/env/app-prod-northwest/dns.tf
@@ -12,15 +12,6 @@ resource "aws_route53_record" "redis-CNAME" {
   records = ["${module.hyperbola-app-redis.redis_endpoint}"]
 }
 
-resource "aws_route53_record" "mysql-CNAME" {
-  zone_id = "${data.aws_route53_zone.aws-dc.id}"
-  name    = "mysql"
-  type    = "CNAME"
-  ttl     = 300
-
-  records = ["${module.hyperbola-app-mysql.mysql_endpoint}"]
-}
-
 resource "aws_route53_record" "mysql2-CNAME" {
   zone_id = "${data.aws_route53_zone.aws-dc.id}"
   name    = "mysql2"
