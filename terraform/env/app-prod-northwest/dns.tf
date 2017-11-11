8a37f6c45d3a2637dbcedf866b5a3e2cf5373536
Ryan Lopopolo <rjl@hyperbo.la>
2017-11-10 20:58:25 -0800

    Remove dependency on cloudflare
    With AWS and CloudFlare DNS, hyperbo.la had 6 NS records
    
    It appears CloudFlare does not fetch all of them when validating
    name servers. They are deactivating DNS for hyperbo.la, so tear
    everything down.
    
    OOB, I have removed CF nameservers from Namecheap

--- terraform/env/app-prod-northwest/dns.tf
+++ terraform/env/app-prod-northwest/dns.tf
@@ -26,35 +26,6 @@ resource "aws_route53_record" "bastion-prod" {
   records = ["${module.network.bastion_public_ip}"]
 }
 
-variable "cloudflare_email" {
-  default = "rjl@hyperbo.la"
-}
-
-variable "cloudflare_token" {}
-
-provider "cloudflare" {
-  email = "${var.cloudflare_email}"
-  token = "${var.cloudflare_token}"
-}
-
-resource "cloudflare_record" "hyperbo_la" {
-  domain  = "hyperbo.la"
-  name    = "hyperbo.la"
-  value   = "${module.hyperbola-app-backend.alb_dns}"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = false
-}
-
-resource "cloudflare_record" "www_hyperbo_la" {
-  domain  = "hyperbo.la"
-  name    = "www"
-  value   = "${module.hyperbola-app-backend.alb_dns}"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = false
-}
-
 resource "aws_route53_record" "hyperbo_la_A" {
   zone_id = "${data.aws_route53_zone.hyperbola-zone.zone_id}"
   name    = "hyperbo.la"
