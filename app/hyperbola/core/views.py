91ae19a6701ac4c5c1843dfd78fe93213df5d197
Ryan Lopopolo <rjl@hyperbo.la>
2017-11-12 23:04:43 -0800

    Remove healthz view (replaced by middleware)

--- app/hyperbola/core/views.py
+++ app/hyperbola/core/views.py
@@ -1,5 +1,3 @@
-from django.http import HttpResponse
-from django.views.generic import View
 from django.views.generic.base import TemplateView
 
 
@@ -9,25 +7,3 @@ class NotFound404View(TemplateView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         return self.render_to_response(context, status=404)
-
-
-class HttpResponseServiceUnavailable(HttpResponse):
-    status_code = 503
-
-
-class ReadinessCheckView(View):
-    """Simple readiness check view to determine DB connection / query."""
-
-    @classmethod
-    def get(cls, request):
-        import django.db
-        del request
-        try:
-            with django.db.connection.cursor() as c:
-                c.execute("SELECT 0")
-        except django.db.Error:
-            return HttpResponseServiceUnavailable("Database health check failed")
-
-        return HttpResponse("OK", content_type="text/plain")
-
-    head = get
