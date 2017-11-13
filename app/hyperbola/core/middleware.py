1cad033a315e8ccb67ce0e8fe53a679d6dedb93a
Ryan Lopopolo <rjl@hyperbo.la>
2017-11-12 22:59:50 -0800

    Add healthz middleware
    Middleware depends on db and cache which means exceptions can be
    thrown if the db goes away because the view healthz code is not
    reached

--- app/hyperbola/core/middleware.py
+++ app/hyperbola/core/middleware.py
@@ -1,5 +1,8 @@
+import logging
 import socket
+import uuid
 
+from django.http import HttpResponse, HttpResponseServerError
 from django.utils.encoding import force_bytes
 
 _FQDN = socket.getfqdn()
@@ -22,3 +25,49 @@ class FQDNMiddleware(object):
                 pass
 
         return response
+
+
+# https://www.ianlewis.org/en/kubernetes-health-checks-django
+class HealthCheckMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.logger = logging.getLogger('healthz')
+        self.cache_key = uuid.uuid4().hex
+
+    def __call__(self, request):
+        if request.path == '/healthz' and request.method in ['GET', 'HEAD']:
+            return self.healthz(request)
+        return self.get_response(request)
+
+    def healthz(self, request):
+        """Return that the server is healthy."""
+        # Connect to each database and do a generic standard SQL query
+        # that doesn't write any data and doesn't depend on any tables
+        # being present.
+        try:
+            from django.db import connections
+            for name in connections:
+                cursor = connections[name].cursor()
+                cursor.execute('SELECT 1;')
+                row = cursor.fetchone()
+                if row is None:
+                    return HttpResponseServerError('KO: db: invalid response')
+        except Exception as e:
+            self.logger.exception(e)
+            return HttpResponseServerError('KO: db: cannot connect to database.')
+
+        # Do a roundtrip SET and GET on a random key/value.
+        # This can effectively check if each is online.
+        try:
+            from django.core.cache import caches
+            for cache in caches.all():
+                cache_value = uuid.uuid4().hex
+                cache.set(self.cache_key, cache_value)
+                result = cache.get(self.cache_key)
+                if result != cache_value:
+                    return HttpResponseServerError('KO: cache: round trip error.')
+        except Exception as e:
+            self.logger.exception(e)
+            return HttpResponseServerError('KO: cache: cannot connect to cache.')
+
+        return HttpResponse('OK')
