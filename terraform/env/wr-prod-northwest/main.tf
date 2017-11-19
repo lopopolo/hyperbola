fcd536ee25c10a784167c37af15e29726962ceaa
Ryan Lopopolo <rjl@hyperbo.la>
2017-11-18 19:50:20 -0800

    Create cloudfront distribution for wr-prod

--- terraform/env/wr-prod-northwest/main.tf
+++ terraform/env/wr-prod-northwest/main.tf
@@ -46,3 +46,64 @@ resource "aws_s3_bucket_object" "image" {
   etag         = "${md5(file("${path.root}/site/WR-124.jpg"))}"
   content_type = "image/jpeg"
 }
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "cloudfront-acm-region"
+}
+
+data "aws_acm_certificate" "website" {
+  provider = "aws.cloudfront-acm-region"
+  domain   = "www.burnfastburnbright.com"
+  statuses = ["ISSUED"]
+}
+
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name = "${aws_s3_bucket.website.bucket_domain_name}"
+    origin_id   = "s3-website"
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "CloudFront for www.burnfastburnbright.com"
+
+  aliases = ["www.burnfastburnbright.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-website"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags {
+    Environment = "wr-prod"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = "${data.aws_acm_certificate.website.arn}"
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+}
