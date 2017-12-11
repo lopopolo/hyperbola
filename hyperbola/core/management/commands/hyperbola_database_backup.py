eefe44df6bae0581b3fdabc7b5fcddb7a230e2d7
Ryan Lopopolo <rjl@hyperbo.la>
2017-12-10 20:24:29 -0800

    Compress json database dumps (4-6x space savings)

--- hyperbola/core/management/commands/hyperbola_database_backup.py
+++ hyperbola/core/management/commands/hyperbola_database_backup.py
@@ -8,6 +8,8 @@ The pretty-printed JSON file is uploaded to the S3 backup bucket.
 
 import datetime
 import io
+import os
+import tarfile
 from pathlib import Path
 
 import boto3
@@ -20,7 +22,7 @@ class Command(BaseCommand):
     help = 'Back up the results of the dumpdata command to S3'
 
     now = datetime.datetime.utcnow()
-    S3_BACKUP_VERSION = getattr(settings, 'HYPERBOLA_S3_BACKUP_VERSION', 'v5')
+    S3_BACKUP_VERSION = getattr(settings, 'HYPERBOLA_S3_BACKUP_VERSION', 'v6')
     S3_BACKUP_TIMESTAMP = now.strftime('%Y-%m-%dT%H%MZ')
     S3_BACKUP_BUCKET = getattr(
         settings,
@@ -33,11 +35,13 @@ class Command(BaseCommand):
         s3 = boto3.resource('s3', settings.AWS_REGION)
 
         dumpdata_file_name = 'hyperbola-app-{}.json'.format(self.S3_BACKUP_TIMESTAMP)
-        dumpdata_file_key = str(self.S3_BACKUP_KEY_PREFIX.joinpath('database', dumpdata_file_name))
+        dumpdata_archive_name = '{}.tar.gz'.format(dumpdata_file_name)
+        dumpdata_file_key = str(self.S3_BACKUP_KEY_PREFIX.joinpath('database',
+                                                                   dumpdata_archive_name))
 
         self.stdout.write('Preparing backup at {}'.format(self.now.isoformat()))
         self.stdout.write('Uploading backup to s3://{}/{}'.format(self.S3_BACKUP_BUCKET, dumpdata_file_key))
-        with io.StringIO() as out:
+        with io.StringIO() as out, io.BytesIO() as outbin, io.BytesIO() as outgz:
             call_command(
                 'dumpdata',
                 natural_foreign=True,
@@ -45,6 +49,16 @@ class Command(BaseCommand):
                 exclude=['sessions', 'contenttypes', 'auth.Permission'],
                 stdout=out
             )
-            s3.Bucket(self.S3_BACKUP_BUCKET).put_object(Key=dumpdata_file_key, Body=out.getvalue())
+            outbin.write(out.getvalue().encode('UTF-8'))
+
+            tarinfo = tarfile.TarInfo(name=dumpdata_file_name)
+            outbin.seek(0, os.SEEK_END)
+            tarinfo.size = outbin.tell()
+            outbin.seek(0, os.SEEK_SET)
+            with tarfile.open(fileobj=outgz, mode='w:gz') as tar:
+                tar.addfile(tarinfo=tarinfo, fileobj=outbin)
+
+            s3.Bucket(self.S3_BACKUP_BUCKET).put_object(Key=dumpdata_file_key,
+                                                        Body=outgz.getvalue())
 
         self.stdout.write(self.style.SUCCESS('Upload completed'))
