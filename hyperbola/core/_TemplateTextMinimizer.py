d7153792771e9c355446804e5dc0c74fb2629c45
Ryan Lopopolo <rjl@hyperbo.la>
2018-02-10 17:48:50 -0800

    Import django-template-minimizer from fork
    https://github.com/lopopolo/django-template-minimizer/commit/5baf85f1fffbad60994c27f52a841592411a770c

---
+++ hyperbola/core/_TemplateTextMinimizer.py
@@ -0,0 +1,162 @@
+"""Copyright (c) 2012 Charles Kaminski (CharlesKaminski@gmail.com)
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE."""
+
+from __future__ import print_function
+
+import re
+from ._ManageMinimizers import get_minimizers
+
+FLAGS = re.IGNORECASE + re.DOTALL
+
+EXCLUDE = r'({#\s*NOMINIFY\s*#})(.*?)({#\s*ENDNOMINIFY\s*#})'
+REMOVE = r'({%\s*COMMENT\s*%})(.*?)({%\s*ENDCOMMENT\s*%})' # Tag Comments
+REMOVE2 = r'({#[^\n\r]+#})' # Other Comments
+DVAR = r'({{[^\n\r]+}})' # Django variable
+DTAG = r'({%[^\n\r]+%})' # Django tag
+SCRIPT = r'(<SCRIPT\b[^>]*>)(.*?)(</SCRIPT>)'
+STYLE = r'(<STYLE\b[^>]*>)(.*?)(</STYLE>)'
+
+RE_EXCLUDE = re.compile(EXCLUDE, flags=FLAGS)
+RE_REMOVE = re.compile(REMOVE, flags=FLAGS)
+RE_REMOVE2 = re.compile(REMOVE2, flags=FLAGS)
+RE_DVAR = re.compile(DVAR, flags=FLAGS)
+RE_DTAG = re.compile(DTAG, flags=FLAGS)
+
+# Will encasing the key in quotes. Will work well with javascript minifiers?
+KEY = '_~%s~_'
+
+HTMLMINIMIZERS = get_minimizers()
+
+def minimize_template_text(text):
+    """Takes a Django template text and returns a minified version.
+
+    Performance is not critical as this function should be run off-line
+    to store minimized templates.
+
+    Use the {# NOMINIFY #} {# ENDNOMINIFY #} comment tags to wrap code you do
+    not want minified.  Suggested uses include wrapping pre, code, and textarea
+    html tags as well as the example above.
+    """
+
+    # Create a list to hold special values temporarily removed
+    # from the text.
+    # format -> [(key, value), ...]
+    word_list = []
+
+    # Remove Excluded text
+    excludes = RE_EXCLUDE.findall(text)
+
+    # Strip out the {# NOMINIFY #} and {# ENDNOMINIFY #} tags
+    excludes_find = [''.join(x) for x in excludes]
+    excludes_replace = [x[1] for x in excludes]
+
+    # replace excluded text with keys and populate word_list
+    text = substitute_text(text, word_list, KEY, excludes_find, excludes_replace)
+
+    # Remove any remaining template comments
+    text = RE_REMOVE.sub('', text)
+    text = RE_REMOVE2.sub('', text)
+
+    # We want to "protect" Django Vars and Tags
+    # then treat scripts and styles after "protecting" Django Vars and tags
+
+    # Extract Django Variables
+    dvars = RE_DVAR.findall(text)
+
+    # replace Django Variables text with keys and populate word_list
+    text = substitute_text(text, word_list, KEY, dvars)
+
+    # Extract django tags
+    dtags = RE_DTAG.findall(text)
+
+    # replace Django Tags text with keys and populate word_list
+    text = substitute_text(text, word_list, KEY, dtags)
+
+    # Run HTML Minimizers
+    def run_html_minimizer(minimizers, content):
+        """Go loop for running HTML minimizers."""
+        if not minimizers:
+            return content
+        minimizers = list(minimizers)
+        to_run = minimizers.pop(0)
+        return run_html_minimizer(minimizers, to_run(content))
+
+    text = run_html_minimizer(HTMLMINIMIZERS, text)
+
+    # put values back into text
+    text = revert_text_keys(text, word_list)
+
+    return text.strip()
+
+def revert_text_keys(text, word_list):
+    """ revert the text keys.  We may have to do this multiple times
+    as some keys may be embeded in other word_list entries"""
+
+    if not text or not word_list:
+        return text
+
+    # If we loop through the word_list 10 times without
+    #   consuming it completely, then something's probably wrong.
+
+    max_iterations = len(word_list) * 10
+
+    for _iteration in range(max_iterations):
+
+        # When the wordlist empties, break the for loop
+        if not word_list:
+            break
+
+        # Take the first entry off the list
+        key, value = word_list.pop(0)
+
+        if key in text:
+            # Replace the key with the value
+            text = text.replace(key, value)
+        else:
+            # Put the entry back at the end of the list
+            word_list.append((key, value))
+    else:
+        # We looped through too many time and
+        # never consumed the whole word_list
+        raise Exception('Minimizer failed to find all embeded variables.\n'
+                        '%s\n%s' % (word_list, text))
+
+    return text
+
+def substitute_text(text, word_list, key_template,
+                    find_list, substitute_list=None):
+    """ Using the text provided, subsitutes the first occurance of each entry
+        in find_list with a key.  Store the key and value in the substitute_list
+        in the word_list.  If no substitute_list is provided, use the value in
+        the find list"""
+    if not substitute_list:
+        substitute_list = find_list
+
+    values_list = zip(find_list, substitute_list)
+
+    for find, substitute in values_list:
+        key = key_template % len(word_list)
+        text = text.replace(find, key, 1)
+        word_list.append((key, substitute))
+
+    return text
---
+++ hyperbola/core/management/commands/minimizetemplates.py
@@ -0,0 +1,192 @@
+"""Copyright (c) 2012 Charles Kaminski (CharlesKaminski@gmail.com)
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE."""
+
+from optparse import make_option
+from pathlib import Path
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from django.template.loaders.app_directories import get_app_template_dirs
+
+from ._TemplateTextMinimizer import minimize_template_text
+
+ARCHIVE = '_minimizer_archive'
+REVERTED = '_reverted_'
+ARCHIVE_EXISTS = ('A minimizer archive folder already exists.\n'
+                  'Check that the following templates are not already '
+                  'minimized: \n%s')
+ARCHIVE_DOESNT_EXIST = ("The below archive folder doesn't exist.\n"
+                        "Check that you shouldn't be reverting the "
+                        "coresponding folder: \n%s")
+
+class Command(BaseCommand):
+    help = '''Use this tool to minimize Django templates after
+development.  This way, your templates are small when they are
+evaluated and the HTML served is already minimized; eliminiating
+any post-processing minimization step.
+
+Use the comment tags {# NOMINIFY #} {# ENDNOMINIFY #} to wrap
+content you do not want minified.
+
+Uses the setting TEMPLATES in your Django settings file and
+    `get_app_template_dirs` to tell the command where to find
+    templates to minimize.
+
+Customization:
+The minimizer command uses its own minimizers for html, style tag embeded
+    css, and script tag embeded javascript. You can override these and chain
+    any number of your own minimizers using settings below.  These
+    settings go in the Django settings file. Custom minimizers must
+    be functions that accept text as a string parameter and return
+    text as a string.
+    JAVASCRIPT_MINIMIZERS = [custom_function1, custom_function2,]
+    CSS_MINIMIZERS        = [custom_function3, custom_function4,]
+    HTML_MINIMIZERS       = [custom_function5, custom_function6,]
+
+    To turn off a minimizer, use the following pattern:
+    f = lambda x: x
+    JAVASCRIPT_MINIMIZER = [f,]
+
+You can tell the minimizer command to disable an aggressive minimizer
+    in the default HTML minimizer chain.  This minimizer normally removes
+    (instead of just collapsing) the remaining space between '>' & '<'
+    character.  Set the following setting to False in your Django
+    settings file to disable this final step:
+    AGGRESSIVE_HTML_MINIMIZER = False
+
+Method - For each template, the minimizer command:
+1. Replaces any {# NOMINIFY #} {# ENDNOMINIFY #} content with
+    a unique identifier and saves the content in memory so that
+    it is excluded from the rest of the process.
+2. Remaining Django comments are removed.
+3. Django tags and django variables are replaced with with unique
+    identifiers.  The tags and variables are saved in memory.
+    This approach "protects" the tags and variables from the
+    minimizers.  It also allows you to use Django tags and variables
+    inside your javascript and CSS without ill effect by the
+    CSS or javascript minimizer.
+4. HTML script tags and content are replaced with unique
+    identifiers. The tags and content are saved in memory for
+    additional processing.  The type attribute for the script tag
+    is checked to see if the script content is javascript.  If no
+    type is provided, then javascript is assumed.  Any javascript
+    is then run through the javascript minimizers.
+5. An almost identical process to step 4 is implemented on the HTML
+    style tags for css.
+6. The remaining text (with the identifiers) is run through the
+    html minimizers.
+7. All of the content saved in memory and associated with unique
+    identifiers are put back.
+8. The original template is moved to an archive folder and replaced
+    with the minimized template.
+
+Limitations:
+The minimizer does not handle script tags inside script tags or
+    style tags inside style tags; an unusual occurance.
+    eg: <script>bla bla <script> bla</script></script>
+The minimizer collapses all white space not in a django tag,
+    django variable, javascript, or inline css.  This includes
+    whitespace inside <pre>, <textarea>, & similar tags, and
+    whitespace inside html attributes.
+Use the {# NOMINIFY #} {# ENDNOMINIFY #} comment tags to overcome
+    these limiations.
+
+'''
+    _option_list = (
+        make_option('-m', '--minimize',
+                    action='store_true', dest='minimize', default=False,
+                    help='Minimize templates.'),)
+
+    def add_arguments(self, parser):
+        option_typemap = {
+            'string': str,
+            'int': int,
+            'float': float
+        }
+        for opt in self._option_list:
+            option = {k: v
+                      for k, v in opt.__dict__.items()
+                      if v is not None}
+            flags = (option.get('_long_opts', []) +
+                     option.get('_short_opts', []))
+            del option['_long_opts']
+            del option['_short_opts']
+            if 'type' in option:
+                opttype = option['type']
+                option['type'] = option_typemap.get(opttype, opttype)
+            parser.add_argument(*flags, **option)
+
+    def handle(self, *args, **options):
+        # Reading template location from settings
+        dirs = list(settings.TEMPLATES[0]['DIRS']) + \
+            list(get_app_template_dirs('templates'))
+
+        if options['minimize']:
+            self.minimize_templates(dirs)
+            self.stdout.write(self.style.SUCCESS('Successfully minimized '
+                                                 'Templates:\n'))
+            for template_dir in dirs:
+                self.stdout.write('%s\n' % template_dir)
+            return 0
+        else:
+            command_name = Path(__file__).name.rstrip('.py')
+            self.print_help(command_name, '')
+
+    def minimize_templates(self, dirs):
+        template_dir_paths = [Path(d) for d in dirs]
+        # Check that the archive folders don't already exist
+        for template_dir in template_dir_paths:
+            archive_dir = template_dir / ARCHIVE
+            if archive_dir.exists():
+                raise CommandError(ARCHIVE_EXISTS % template_dir)
+
+        paths = []
+        # Walk the directories to build a list of files to minimize
+        # Currently not following symbolic links
+        for template_dir in template_dir_paths:
+            for template in template_dir.rglob('*.html'):
+                paths.append((template_dir, template))
+
+        # Minimize the files
+        num_files, before, after = 0, 0, 0
+        for template_root, template_path in paths:
+            original = template_path.read_text()
+            minimized = minimize_template_text(original)
+
+            num_files = num_files + 1
+            before = before + len(original)
+            after = after + len(minimized)
+
+            archive_path = template_root / ARCHIVE
+            try:
+                archive_path.mkdir()
+            except FileExistsError:
+                pass
+            template_path.rename(archive_path / template_path.name)
+            template_path.write_text(minimized)
+
+        self.stdout.write('Files:    %s' % num_files)
+        self.stdout.write('Before:   %s' % before)
+        self.stdout.write('After:    %s' % after)
+        self.stdout.write(
+            'Decrease: {:.0%}'.format((before - after) / float(before)))
