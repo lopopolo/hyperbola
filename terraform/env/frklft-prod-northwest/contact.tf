25c47a8f5bb649b23eeb9a1575a68b0b27acdd0c
Ryan Lopopolo <rjl@hyperbo.la>
2018-04-07 03:23:48 -0700

    Extract contact form js code from cloudformation template

--- terraform/env/frklft-prod-northwest/contact.tf
+++ terraform/env/frklft-prod-northwest/contact.tf
@@ -1,6 +1,34 @@
 variable "contact_email" {}
 variable "recaptcha_secret_key" {}
 
+data "template_file" "cloudformation-body" {
+  template = "$${before}$${lambda_code}$${after}"
+
+  vars {
+    before = "${
+      element(
+        split(
+          "lambda-code-42B3B8CA-0C02-477E-93A2-5C33D2E88383",
+          file("${path.module}/contact-form-cloudformation-stack.yml")
+        ),
+      0)}"
+
+    lambda_code = "${
+      indent(
+        10,
+        "${file("${path.root}/../../../frklft/src/index.js")}"
+      )}"
+
+    after = "${
+      element(
+        split(
+          "lambda-code-42B3B8CA-0C02-477E-93A2-5C33D2E88383",
+          file("${path.module}/contact-form-cloudformation-stack.yml")
+        ),
+      1)}"
+  }
+}
+
 resource "aws_cloudformation_stack" "contact-form" {
   name         = "FRKLFT-Contact-Us"
   capabilities = ["CAPABILITY_IAM"]
@@ -10,7 +38,7 @@ resource "aws_cloudformation_stack" "contact-form" {
     ReCaptchaSecret = "${var.recaptcha_secret_key}"
   }
 
-  template_body = "${file("${path.module}/contact-form-cloudformation-stack.yml")}"
+  template_body = "${data.template_file.cloudformation-body.rendered}"
 }
 
 output "contact_api_url" {
