2873cff090f993bc11dbb365b0fe8c3f0e867d5d
Ryan Lopopolo <rjl@hyperbo.la>
2018-05-30 09:38:17 -0700

    mysql works

--- docker/fixtures-entrypoint.sh
+++ docker/fixtures-entrypoint.sh
@@ -9,7 +9,8 @@ env "$(venv/bin/dotenv get DB_HOST)" \
     "$(venv/bin/dotenv get DB_USER)" \
     "$(venv/bin/dotenv get DB_PASSWORD)" \
     "$(venv/bin/dotenv get DB_NAME)" \
-    sh -c 'mysql --host="$DB_HOST" --port="$DB_PORT" --user="$DB_USER" --password="$DB_PASSWORD" -e "ALTER DATABASE $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"'
+    sh -c 'mysql --host="$DB_HOST" --port="$DB_PORT" --user="$DB_USER" --password="$DB_PASSWORD" -e \
+        "ALTER DATABASE $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"'
 
 venv/bin/aws s3 cp \
     s3://hyperbola-app-backup-local/v5/local/database/hyperbola-app-2017-12-03T0126Z.json \
