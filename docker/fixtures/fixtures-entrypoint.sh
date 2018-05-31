0b6b9c6dc47dda4a2e17c571718a38f6d06716be
Ryan Lopopolo <rjl@hyperbo.la>
2018-05-30 22:50:41 -0700

    Automated local builds and deploys using docker and make

--- docker/fixtures/fixtures-entrypoint.sh
+++ docker/fixtures/fixtures-entrypoint.sh
@@ -16,6 +16,5 @@ venv/bin/aws s3 cp \
     s3://hyperbola-app-backup-local/v5/local/database/hyperbola-app-2017-12-03T0126Z.json \
     hyperbola-fixtures.json
 
-venv/bin/python manage.py migrate
 venv/bin/python manage.py loaddata hyperbola-fixtures.json
 venv/bin/python manage.py createcachetable
