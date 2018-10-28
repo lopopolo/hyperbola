ef3cd4e23f8e54133396fc8291b611c003a0a711
Ryan Lopopolo <rjl@hyperbo.la>
2018-10-27 19:59:49 -0700

    Add blog index

--- hyperbola/blog/urls.py
+++ hyperbola/blog/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from . import views
 
 app_name = "blog"
-urlpatterns = [path("<slug:slug>/", views.post, name="post")]
+urlpatterns = [path("", views.index, name="index"), path("<slug:slug>/", views.post, name="post")]
--- hyperbola/blog/views.py
+++ hyperbola/blog/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
+def index(request):
+    posts = Post.objects.order_by("-publish_date", "-pk")
+    return render(request, "blog_index.html", {"posts": posts})
+
+
 def post(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, "blog_post.html", {"post": post})
