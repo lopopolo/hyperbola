f8ae83a7390a16e54c4fe8f517462d5a18e35b79
Ryan Lopopolo <rjl@hyperbo.la>
2018-11-10 22:11:24 -0800

    Bump version: 0.149.0 → 0.149.1

--- hyperbola/__init__.py
+++ hyperbola/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ("settings", "urls", "wsgi")
-__version__ = "0.149.0"
+__version__ = "0.149.1"
--- terraform/modules/hyperbola/app/backend/main.tf
+++ terraform/modules/hyperbola/app/backend/main.tf
@@ -159,7 +159,7 @@ data "aws_ami" "backend" {
 
   filter {
     name   = "tag:Version"
-    values = ["0.149.0"]
+    values = ["0.149.1"]
   }
 }
 
@@ -194,7 +194,7 @@ resource "aws_launch_template" "backend" {
     tags {
       Name        = "${var.name}-backend"
       Environment = "${var.env}"
-      Version     = "0.149.0"
+      Version     = "0.149.1"
     }
   }
 
